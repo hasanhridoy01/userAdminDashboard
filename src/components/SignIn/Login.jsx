25bef7185acc902d6cb64dfc39975e56c30f57b1
hasanhridoy01 <hasanhridoy1367@gmail.com>
2024-01-20 04:03:27 -0800

    login page edit done

--- src/components/SignIn/Login.jsx
+++ src/components/SignIn/Login.jsx
@@ -138,12 +138,10 @@ const Login = () => {
                     id="email"
                     required
                     ref={emailref}
-                    className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                    className="input input-bordered input-success w-full max-w-md"
                     placeholder="Email address"
                   />
-                  <label className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">
-                    Email address
-                  </label>
+                  
                 </div>
 
                 <div className="relative mb-6" data-te-input-wrapper-init>
@@ -152,12 +150,9 @@ const Login = () => {
                     name="password"
                     id="password"
                     required
-                    className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                    className="input input-bordered input-success w-full max-w-md"
                     placeholder="Password"
                   />
-                  <label className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">
-                    Password
-                  </label>
                 </div>
 
                 <div className="mb-6 flex items-center justify-between">
